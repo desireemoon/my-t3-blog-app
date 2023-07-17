737e13fb2c1fbf5251298cbdd59018632b1ab1b2
desireemoon <desiree.dewysocki@gmail.com>
2023-07-17 11:54:57 -0400

    Added post component

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,18 @@
+// @ts-check
+
 /**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
+ * This is especially useful for Docker builds.
  */
-await import("./src/env.mjs");
+!process.env.SKIP_ENV_VALIDATION && (await import("./src/env.mjs"));
 
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
 
   /**
-   * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
-   * out.
+   * If you have the "experimental: { appDir: true }" setting enabled, then you
+   * must comment the below `i18n` config out.
    *
    * @see https://github.com/vercel/next.js/issues/41980
    */
@@ -19,8 +21,10 @@ const config = {
     defaultLocale: "en",
   },
   experimental: {
-  appDir: true,
-},
+    appDir: true,
+  },
+  images: {
+    domains: ["avatars.githubusercontent.com"],
+  },
 };
-
 export default config;
